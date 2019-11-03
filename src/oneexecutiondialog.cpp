9073cad4d09540ffc3e2aa62f1b46cb929adf546
Ivan Bazanov <i.bazanov@yandex.ru>
2019-11-03 17:26:22 +0300

    Warnings on wrong execution data

--- src/oneexecutiondialog.cpp
+++ src/oneexecutiondialog.cpp
@@ -4,94 +4,101 @@
 #include <QLabel>
 #include <QDateEdit>
 #include <QPlainTextEdit>
+#include <QMessageBox>
 
 OneExecutionDialog::OneExecutionDialog(QWidget *parent, Database& db, QDate date):
-    QDialog(parent),
-    ui(new Ui::OneExecutionDialog),
-    _db(db)
+	QDialog(parent),
+	ui(new Ui::OneExecutionDialog),
+	_db(db)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
 
-    loadExercisesList();
+	loadExercisesList();
 
-    QDateEdit* dateEdit = findChild<QDateEdit*>("dateExecution");
-    dateEdit->setDate(date);
+	QDateEdit* dateEdit = findChild<QDateEdit*>("dateExecution");
+	dateEdit->setDate(date);
 }
 
 OneExecutionDialog::~OneExecutionDialog()
 {
-    delete ui;
+	delete ui;
 }
 
 void OneExecutionDialog::selectExercise(QListWidgetItem* item)
 {
-    int id = item->data(Qt::UserRole).toInt();
-    QStringList paramNames, paramUnits;
-    QString comment, lastComment;
-    _db.fetchExInfo(id, paramNames, paramUnits, comment, lastComment);
+	int id = item->data(Qt::UserRole).toInt();
+	QStringList paramNames, paramUnits;
+	QString comment, lastComment;
+	_db.fetchExInfo(id, paramNames, paramUnits, comment, lastComment);
 
-    QTableWidget* params = findChild<QTableWidget*>("tableParams");
-    params->setRowCount(paramNames.count());
-    for (int i = 0; i < paramNames.count(); ++i)
-    {
-        params->setItem(i, 0, new QTableWidgetItem(paramNames[i]));
-        params->item(i, 0)->setFlags(Qt::NoItemFlags);
-        params->setItem(i, 1, new QTableWidgetItem("0"));
-        params->item(i, 1)->setFlags(Qt::ItemIsEnabled | Qt::ItemIsEditable | Qt::ItemIsSelectable);
-        params->setItem(i, 2, new QTableWidgetItem(paramUnits[i]));
-        params->item(i, 2)->setFlags(Qt::NoItemFlags);
-    }
+	QTableWidget* params = findChild<QTableWidget*>("tableParams");
+	params->setRowCount(paramNames.count());
+	for (int i = 0; i < paramNames.count(); ++i)
+	{
+		params->setItem(i, 0, new QTableWidgetItem(paramNames[i]));
+		params->item(i, 0)->setFlags(Qt::NoItemFlags);
+		params->setItem(i, 1, new QTableWidgetItem("0"));
+		params->item(i, 1)->setFlags(Qt::ItemIsEnabled | Qt::ItemIsEditable | Qt::ItemIsSelectable);
+		params->setItem(i, 2, new QTableWidgetItem(paramUnits[i]));
+		params->item(i, 2)->setFlags(Qt::NoItemFlags);
+	}
 
-    QLabel* excomments = findChild<QLabel*>("labelComments");
-    excomments->setText(comment);
+	QLabel* excomments = findChild<QLabel*>("labelComments");
+	excomments->setText(comment);
 
-    QPlainTextEdit* thisComment = findChild<QPlainTextEdit*>("textComment");
-    thisComment->setPlainText(lastComment);
+	QPlainTextEdit* thisComment = findChild<QPlainTextEdit*>("textComment");
+	thisComment->setPlainText(lastComment);
 }
 
 void OneExecutionDialog::loadExercisesList()
 {
-    IdToString exs = _db.fetchExList(true);
-    QListWidget *lst = findChild<QListWidget*>("listExercises");
-    for (IdToString::const_iterator i = exs.begin(); i != exs.end(); ++i)
-    {
-        QListWidgetItem* pItem = new QListWidgetItem(i.value());
-        pItem->setData(Qt::UserRole, i.key());
-        lst->addItem(pItem);
-    }
-    lst->sortItems();
+	IdToString exs = _db.fetchExList(true);
+	QListWidget *lst = findChild<QListWidget*>("listExercises");
+	for (IdToString::const_iterator i = exs.begin(); i != exs.end(); ++i)
+	{
+		QListWidgetItem* pItem = new QListWidgetItem(i.value());
+		pItem->setData(Qt::UserRole, i.key());
+		lst->addItem(pItem);
+	}
+	lst->sortItems();
 }
 
 void OneExecutionDialog::accept()
 {
-    QDateEdit* dateEdit = findChild<QDateEdit*>("dateExecution");
+	QDateEdit* dateEdit = findChild<QDateEdit*>("dateExecution");
 
-    QListWidget *lst = findChild<QListWidget*>("listExercises");
-    if (lst->selectedItems().size() == 0)
-    {
-        return;
-    }
-    int id = lst->selectedItems()[0]->data(Qt::UserRole).toInt();
+	QListWidget *lst = findChild<QListWidget*>("listExercises");
+	if (lst->selectedItems().size() == 0)
+	{
+		QMessageBox::warning(this,"Недопустимый параметр", "Не выбрано упражнение");
+		return;
+	}
+	int id = lst->selectedItems()[0]->data(Qt::UserRole).toInt();
 
-    double p1 = 0, p2 = 0, p3 = 0;
-    QTableWidget* params = findChild<QTableWidget*>("tableParams");
-    if (params->rowCount() > 0)
-    {
-        p1 = params->item(0, 1)->text().toDouble();
-    }
-    if (params->rowCount() > 1)
-    {
-        p2 = params->item(1, 1)->text().toDouble();
-    }
-    if (params->rowCount() > 2)
-    {
-        p3 = params->item(2, 1)->text().toDouble();
-    }
+	double p1 = 0, p2 = 0, p3 = 0;
+	QTableWidget* params = findChild<QTableWidget*>("tableParams");
+	if (params->rowCount() > 0)
+	{
+		p1 = params->item(0, 1)->text().toDouble();
+		if (p1 < 0.0000001)
+		{
+			QMessageBox::warning(this,"Недопустимый параметр", "Не заполнено количество");
+			return;
+		}
+	}
+	if (params->rowCount() > 1)
+	{
+		p2 = params->item(1, 1)->text().toDouble();
+	}
+	if (params->rowCount() > 2)
+	{
+		p3 = params->item(2, 1)->text().toDouble();
+	}
 
-    int retId = 0;
+	int retId = 0;
 
-    QPlainTextEdit* thisComment = findChild<QPlainTextEdit*>("textComment");
+	QPlainTextEdit* thisComment = findChild<QPlainTextEdit*>("textComment");
 
-    _db.addExecution(dateEdit->date(), id, p1, p2, p3, thisComment->toPlainText(), retId);
-    QDialog::accept();
+	_db.addExecution(dateEdit->date(), id, p1, p2, p3, thisComment->toPlainText(), retId);
+	QDialog::accept();
 }
